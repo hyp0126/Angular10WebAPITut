f6148e8c725293db659eb8e54d5db257bfe0fa35
yhong <hyp0126@gmail.com>
2021-01-28 22:59:32 -0500

    change api address -> EF address (api/Departments, api/Employees)

--- ui/angular10/src/app/shared.service.ts
+++ ui/angular10/src/app/shared.service.ts
@@ -13,45 +13,45 @@ readonly PhotoUrl = "http://localhost:53535/Photos/";
   constructor(private http:HttpClient) { }
 
   getDepList():Observable<any[]>{
-    return this.http.get<any>(this.APIUrl+'/department');
+    return this.http.get<any>(this.APIUrl+'/Departments');
   }
 
   addDepartment(val:any){
-    return this.http.post(this.APIUrl+'/Department',val);
+    return this.http.post(this.APIUrl+'/Departments',val);
   }
 
   updateDepartment(val:any){
-    return this.http.put(this.APIUrl+'/Department',val);
+    return this.http.put(this.APIUrl+'/Departments',val);
   }
 
   deleteDepartment(val:any){
-    return this.http.delete(this.APIUrl+'/Department/'+val);
+    return this.http.delete(this.APIUrl+'/Departments/'+val);
   }
 
 
   getEmpList():Observable<any[]>{
-    return this.http.get<any>(this.APIUrl+'/Employee');
+    return this.http.get<any>(this.APIUrl+'/Employees');
   }
 
   addEmployee(val:any){
-    return this.http.post(this.APIUrl+'/Employee',val);
+    return this.http.post(this.APIUrl+'/Employees',val);
   }
 
   updateEmployee(val:any){
-    return this.http.put(this.APIUrl+'/Employee',val);
+    return this.http.put(this.APIUrl+'/Employees',val);
   }
 
   deleteEmployee(val:any){
-    return this.http.delete(this.APIUrl+'/Employee/'+val);
+    return this.http.delete(this.APIUrl+'/Employees/'+val);
   }
 
 
   UploadPhoto(val:any){
-    return this.http.post(this.APIUrl+'/Employee/SaveFile',val);
+    return this.http.post(this.APIUrl+'/Employees/SaveFile',val);
   }
 
   getAllDepartmentNames():Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl+'/Employee/GetAllDepartmentNames');
+    return this.http.get<any[]>(this.APIUrl+'/Employees/GetAllDepartmentNames');
   }
 
 }
